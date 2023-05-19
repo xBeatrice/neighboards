3536d882dbd3fde203ba4939fd488354dbb5ab22
Beatrice <130166341+xBeatrice@users.noreply.github.com>
2023-05-19 20:26:43 +0300

    Add files via upload

--- src/helpers/iterations.js
+++ src/helpers/iterations.js
@@ -1,6 +1,32 @@
-const iterations = [];
+const iterations = [
+  {
+    id: 1,
+    capacity: { 123: 40, 345: 35 },
+  },
+  {
+    id: 2,
+    capacity: { 123: 40, 345: 35 },
+  },
+  {
+    id: 3,
+    capacity: { 123: 40, 345: 35 },
+  },
+  {
+    id: 4,
+    capacity: { 123: 40, 345: 35 },
+  },
+  {
+    id: 5,
+    capacity: { 123: 40, 345: 35 },
+  },
+  {
+    id: 6,
+    capacity: { 123: 40, 345: 35 },
+  },
+  {
+    id: 7,
+    capacity: { 123: 40, 345: 35 },
+  },
+];
 
-for (let i = 1; i < 101; i++) {
-  iterations.push(i);
-}
 export { iterations };
